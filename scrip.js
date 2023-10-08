7290fc2c55ee88f82b96afde9ca0d7f6c4411965
DianaMaciasP <dluciamacias20@gmail.com>
2023-10-08 13:34:50 -0500

    Initial commit

---
+++ scrip.js
@@ -0,0 +1,21 @@
+//alert("Hola este es mi Javascript");
+//let nombre = "Diana"
+//nombre="lucia"
+//var nombre1="diana"
+//const nombre2="diana"
+
+//console.log(nombre);
+//console.log(nombre1);
+//console.log(nombre2);
+ let contenidotitulo = "nombre";
+
+let titulo = document.querySelector(".logo.fuente")
+titulo.innerHTML = contenidotitulo;
+
+//condicionales
+
+let textotitulo = titulo.innertext;
+console.log(textotitulo);
+
+
+
